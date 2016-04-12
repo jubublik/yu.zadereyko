d9ffa9fec31d13adc743763abbafe05302345104
Юлия Задерейко <jubu@mail.ru>
2016-04-12 18:17:26 +0600

    Lesson#1

---
+++ azaza/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Ввести с консоли 2 числа, вывести их на консоль
+            int a;
+            int b;
+            int c;
+
+            // a = Convert.ToInt32(Console.ReadLine());
+            // b = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine(a + b);
+
+            // Ввести с консоли 2 числа: n и i , вывести значение i-го бита числа n
+
+            // a = Convert.ToInt32(Console.ReadLine());
+            // b = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine((a >> b) & 1);
+
+            // Ввести с консоли число. Обнулить последний бит этого числа. Вывести на консоль
+
+              a = Convert.ToInt32(Console.ReadLine());
+              b = a >> 1;
+              c = b << 1;
+
+              Console.WriteLine(c);
+
+        }
+    }
+}
