d1c812359ed79ed0adee97ecaa04c853ad7e5a21
Юлия Задерейко <jubu@mail.ru>
2016-04-26 16:29:13 +0600

    На занятии , 4ый урок

---
+++ Lesson4/Phone.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    //Создать класс “Телефон”, который содержит следующие данные: код города, номер телефона, которые не могут быть изменены. 
+    //Добавить метод, который возвращает строку вида “(999) 999999” или “999999”, если код города отсутствует.
+
+    class Phone
+    {
+        readonly string Code;
+        readonly string Number;
+
+        public Phone(string code, string number)
+        {
+            Code = code;
+            Number = number;
+        }
+        public string GetPhone()
+        {
+            string result;
+
+            if (Code != "")
+            {
+                result = String.Format("({0}) {1}", Code, Number);
+            }
+            else
+            {
+                result = Number;
+            }
+            return result;
+        }
+    }
+
+
+
+}
---
+++ Lesson4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone a = new Phone("999","999999");
+            a.GetPhone();
+        }
+    }  
+}
